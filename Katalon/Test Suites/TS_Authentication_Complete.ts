8fdac85429aa35b6178c20410d3a9259221dd9a3
AhmedSherif100 <ahmedsheriff122@gmail.com>
2026-01-02 02:31:00 +0200

    Added test suite for all authentication test cases

---
+++ Katalon/Test Suites/TS_Authentication_Complete.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Complete Authentication Microservice Test Suite - All Login, Register, Forgot Password Tests</description>
+   <name>TS_Authentication_Complete</name>
+   <tag>authentication, smoke, regression</tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ts-auth-complete-001</testSuiteGuid>
+   <testCaseLink>
+      <guid>ts-link-001</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Login/TC_AUTH_001_Login_Valid_Credentials</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-002</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Login/TC_AUTH_002_Login_Invalid_Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-003</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Login/TC_AUTH_003_Login_Incorrect_Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-004</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Login/TC_AUTH_004_Login_Empty_Fields</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-005</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Login/TC_AUTH_005_Forgot_Password_Link</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-006</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Register/TC_AUTH_006_Register_Valid_User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-007</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Register/TC_AUTH_007_Register_Valid_Developer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-008</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Register/TC_AUTH_008_Register_Duplicate_Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-009</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Register/TC_AUTH_009_Register_Weak_Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-010</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Register/TC_AUTH_010_Register_Missing_Fields</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-011</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/ForgotPassword/TC_AUTH_011_Forgot_Password_Valid_Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-012</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/ForgotPassword/TC_AUTH_012_Forgot_Password_Invalid_Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-013</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/ForgotPassword/TC_AUTH_013_Forgot_Password_Empty_Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-014</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Authentication/ResetPassword/TC_AUTH_014_Reset_Password_Valid_Token</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-015</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Authentication/ResetPassword/TC_AUTH_015_Reset_Password_Mismatch</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ts-link-016</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/ResetPassword/TC_AUTH_016_Reset_Password_Invalid_Token</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
